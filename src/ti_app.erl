668d08284013b779230082eb7865f64636e68659
unknown <cn569363@testnbwin732.chn.agilent.com>
2013-03-04 20:18:37 +0800

    no info

--- src/ti_app.erl
+++ src/ti_app.erl
@@ -23,22 +23,27 @@ start(_StartType, StartArgs) ->
 	[DefPort, DefPortMan, DefDB, DefPortDB] = StartArgs,
 	ets:new(msgservertable,[set,public,named_table,{keypos,1},{read_concurrency,true},{write_concurrency,true}]),
 	ets:insert(serverstatetable,{dbconncount,0}),
+	% start DB client
 	case ti_sup_db:start_link(DefDB, DefPortDB) of
         {ok, PidDB} ->
             ets:insert(serverstatetable,{dbsuppid,PidDB}),
             ti_sup:start_child(),
+			% start management server 
 			case gen_tcp:listen(DefPortMan, [{active, true}]) of
 				{ok, LSockMan} ->
 				    case ti_sup_man:start_link(LSockMan) of
 				        {ok,PidMan} ->
             				ets:insert(serverstatetable,{mansuppid,PidMan}),
 				            ti_sup:start_child(LSockMan),
+							% start VDR server
 							case gen_tcp:listen(DefPort, [{active, true}]) of
 								{ok, LSock} ->
 								    case ti_sup:start_link(LSock) of
 								        {ok, Pid} ->
 		            						ets:insert(serverstatetable,{suppid,Pid}),
 								            ti_sup:start_child(LSock),
+											% start monitor server
+											% not implemented yet
 											ok;
 								        _ ->
 											error_logger:error_msg("Cannot start server for VDR.~nExit.~n"),
