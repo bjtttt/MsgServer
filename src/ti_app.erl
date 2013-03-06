870be6abba65567c517d32add43fe325e1b84d9a
unknown <cn569363@msnetdev.chn.agilent.com>
2013-03-06 17:26:06 +0800

    can work now, mssage cannot display

--- src/ti_app.erl
+++ src/ti_app.erl
@@ -22,47 +22,56 @@ start() ->
 %%  mantable : 
 %%
 start(_StartType, StartArgs) ->
-	[DefPort, DefPortMan, DefDB, DefPortDB, DefPortMon] = StartArgs,
-	ets:new(msgservertable,[set,public,named_table,{keypos,1},{read_concurrency,true},{write_concurrency,true}]),
-	ets:new(vdrinittable,[set,public,named_table,{keypos,1},{read_concurrency,true},{write_concurrency,true}]),
-	ets:new(vdrtable,[set,public,named_table,{keypos,1},{read_concurrency,true},{write_concurrency,true}]),
-	ets:new(mantable,[set,public,named_table,{keypos,1},{read_concurrency,true},{write_concurrency,true}]),
-	error_logger:error_info("Server tables are ok.~n"),
-	% make sure that msgserver can get stop message from any internal process?
-    PidApp = self(),
-    ets:insert(msgservertable, {apppid,PidApp}),
-	error_logger:error_info("Server application PID : ~p~n",[PidApp]),
-	PidAppMsg = spawn(fun() -> app_message_processor() end),
-    ets:insert(serverstatetable,{appmsgpid, PidAppMsg}),
- 	error_logger:error_info("Server application message PID : ~p~n",[PidAppMsg]),
-	% start DB client
-	case gen_tcp:listen(DefPortDB, [{active,true}]) of
-		{ok, LSock} ->
-			error_logger:error_info("Database starts listening.~n"),
-			case ti_sup_db:start_link(LSock, DefDB, DefPortDB) of
-		        {ok, Pid} ->
-					error_logger:error_info("Database supervisor starts.~n"),
-		            ets:insert(serverstatetable, {supdbpid, Pid}),
-		            ets:insert(serverstatetable, {supdblsock, LSock}),
-		            ti_sup_db:start_child(),
-					% start management server, VDR server & monitor server
-					start_server_man(DefPortMan, DefPort, DefPortMon),
-					error_logger:error_info("Msg server starts.~n");
-				ignore ->
-		            exit("Cannot start connetion to DB : ignore~nExit.~n");
-		        {error, Error} ->
-					case Error of
-						{already_started, Pid} ->
-		            		exit("Cannot start connetion to DB : already started - ~p~nExit.~n", [Pid]);
-						{shutdown, Term} ->
-		            		exit("Cannot start connetion to DB : shutdown - ~p~nExit.~n", [Term]);
-						Term ->
-							exit("Cannot start connetion to DB : ~p~nExit.~n", [Term])
-					end
-		    end;
-		{error, Reason} ->
-			exit("Cannot start connetion to DB : ~p~nExit.~n", [Reason])
-	end.
+	try
+		[DefPort, DefPortMan, DefDB, DefPortDB, DefPortMon] = StartArgs,
+		ets:new(msgservertable,[set,public,named_table,{keypos,1},{read_concurrency,true},{write_concurrency,true}]),
+		ets:new(vdrinittable,[set,public,named_table,{keypos,1},{read_concurrency,true},{write_concurrency,true}]),
+		ets:new(vdrtable,[set,public,named_table,{keypos,1},{read_concurrency,true},{write_concurrency,true}]),
+		ets:new(mantable,[set,public,named_table,{keypos,1},{read_concurrency,true},{write_concurrency,true}]),
+		error_logger:error_msg("Server tables are ok.~n"),
+		% make sure that msgserver can get stop message from any internal process?
+	    PidApp = self(),
+	    ets:insert(msgservertable, {apppid,PidApp}),
+		error_logger:error_msg("Server application PID : ~p~n",[PidApp]),
+		PidAppMsg = spawn(fun() -> app_message_processor() end),
+	    ets:insert(msgservertable,{appmsgpid, PidAppMsg}),
+	 	error_logger:error_msg("Server application message PID : ~p~n",[PidAppMsg]),
+		% start DB client
+		case gen_tcp:listen(DefPortDB, [{active,true}]) of
+			{ok, LSock} ->
+				error_logger:error_msg("Database starts listening.~n"),
+				case ti_sup_db:start_link(LSock, DefDB, DefPortDB) of
+			        {ok, Pid} ->
+						error_logger:error_msg("Database supervisor starts.~n"),
+			            ets:insert(msgservertable, {supdbpid, Pid}),
+			            ets:insert(msgservertable, {supdblsock, LSock}),
+			            ti_sup_db:start_child(),
+						% start management server, VDR server & monitor server
+						start_server_man(DefPortMan, DefPort, DefPortMon),
+						error_logger:error_msg("Msg server starts.~n");
+					ignore ->
+			            exit("Cannot start connetion to DB : ignore~nExit.~n");
+			        {error, Error} ->
+						case Error of
+							{already_started, Pid} ->
+			            		exit("Cannot start connetion to DB : already started - ~p~nExit.~n", [Pid]);
+							{shutdown, Term} ->
+			            		exit("Cannot start connetion to DB : shutdown - ~p~nExit.~n", [Term]);
+							Term ->
+								exit("Cannot start connetion to DB : ~p~nExit.~n", [Term])
+						end
+			    end;
+			{error, Reason} ->
+				exit("Cannot start connetion to DB : ~p~nExit.~n", [Reason])
+		end
+	catch
+		error:AppError ->
+			exit("Server application fails (error) : ~p~nExit.~n", AppError);
+		throw:AppThrow ->
+			exit("Server application fails (throw) : ~p~nExit.~n", AppThrow);
+		exit:AppReason ->
+			exit("Server application fails (exit) : ~p~nExit.~n", AppReason)
+	end.			
 
 %%%
 %%% Start management server
@@ -71,11 +80,13 @@ start(_StartType, StartArgs) ->
 start_server_man(PortMan, Port, PortMon) ->
 	case gen_tcp:listen(PortMan, [{active, true}]) of
 		{ok, LSock} ->
+			error_logger:error_msg("Management starts listening.~n"),
 		    case ti_sup_man:start_link(LSock) of
 		        {ok,Pid} ->
-    				ets:insert(serverstatetable,{supmanpid,Pid}),
-    				ets:insert(serverstatetable,{supmanlsock,LSock}),
-		            ti_sup:start_child(LSock),
+					error_logger:error_msg("Management supervisor starts.~n"),
+    				ets:insert(msgservertable,{supmanpid,Pid}),
+    				ets:insert(msgservertable,{supmanlsock,LSock}),
+		            ti_sup_man:start_child(),
 					% start VDR server & monitor server
 					start_server(Port, PortMon);
 				ignore ->
@@ -101,11 +112,13 @@ start_server_man(PortMan, Port, PortMon) ->
 start_server(Port, PortMon) ->
 	case gen_tcp:listen(Port, [{active, true}]) of
 		{ok, LSock} ->
+			error_logger:error_msg("VDR starts listening.~n"),
 		    case ti_sup:start_link(LSock) of
 		        {ok, Pid} ->
-					ets:insert(serverstatetable,{suppid,Pid}),
-					ets:insert(serverstatetable,{suplsock,LSock}),
-		            ti_sup:start_child(LSock),
+					error_logger:error_msg("VDR supervisor starts.~n"),
+					ets:insert(msgservertable,{suppid,Pid}),
+					ets:insert(msgservertable,{suplsock,LSock}),
+		            ti_sup:start_child(),
 					% start monitor server
 					start_server_mon(PortMon);
 				ignore ->
@@ -130,11 +143,13 @@ start_server(Port, PortMon) ->
 start_server_mon(Port) ->
 	case gen_tcp:listen(Port, [{active, true}]) of
 		{ok, LSock} ->
+			error_logger:error_msg("Monitor starts listening.~n"),
 		    case ti_sup_mon:start_link(LSock) of
 		        {ok, Pid} ->
-					ets:insert(serverstatetable,{supmonpid,Pid}),
-					ets:insert(serverstatetable,{supmonlsock,LSock}),
-		            ti_sup_mon:start_child(LSock);
+					error_logger:error_msg("Monitor supervisor starts.~n"),
+					ets:insert(msgservertable,{supmonpid,Pid}),
+					ets:insert(msgservertable,{supmonlsock,LSock}),
+		            ti_sup_mon:start_child();
 				ignore ->
 		            exit("Cannot start server for monitor : ignore~nExit.~n");
 		        {error, Error} ->
@@ -152,7 +167,7 @@ start_server_mon(Port) ->
 	end.
 
 stop(_State) ->
-	error_logger:error_info("Msg server stops.~n").
+	error_logger:error_msg("Msg server stops.~n").
 
 %%%
 %%% Application get stop message.
