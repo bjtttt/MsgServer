e799918e2ba9a900e736d3097d629b27841642e4
unknown <cn569363@msnetdev.chn.agilent.com>
2013-03-06 13:53:24 +0800

    redesign db client

--- src/ti_common.hrl
+++ src/ti_common.hrl
@@ -7,4 +7,8 @@
 -define(DEF_DB, "127.0.0.1").
 -define(DEF_PORT_DB, 6002).
 
--define(DB_CONN_CNT_MAX,(10)).
+%%% DB_SUP_MAX and DB_SUP_WITHIN are use in DB Supervisor for DB client restart mechanism
+%%% In development, they are 0 and 1 to make debug more easy and efficient.
+%%% When release, they should be 10 and 10 OR other better values
+-define(DB_SUP_MAX,(0)).
+-define(DB_SUP_WITHIN,(1)).
--- src/ti_sup_db.erl
+++ src/ti_sup_db.erl
@@ -6,6 +6,8 @@
 
 -behaviour(supervisor).
 
+-include("ti_common.hrl").
+
 %% API
 -export([start_link/2, start_child/0]).
 
@@ -26,5 +28,5 @@ init([DB, Port]) ->
     Server = {ti_client_db, {ti_client_db, start_link, [DB, Port]},
               temporary, brutal_kill, worker, [ti_client_db]},
     Children = [Server],
-    RestartStrategy = {simple_one_for_one, 0, 1},
+    RestartStrategy = {simple_one_for_one, ?DB_SUP_MAX, ?DB_SUP_WITHIN},
     {ok, {RestartStrategy, Children}}.
