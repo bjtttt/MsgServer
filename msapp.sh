8be6f047a47e12ed9e32d47134ad22218c5126ef
GZT <guozhitao@yahoo.cn>
2013-07-26 14:14:14 +0800

    new

---
+++ msapp.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+# rename this file to "simple_cache" and set the executable flag,
+# then copy it into the bin directory of your release package
+# (update the erts version number below to match your release)
+#./erts-5.7.4/bin/erl \
+#    -sname cache \
+#    -boot ./releases/0.1.0/start \
+#    -config ./releases/0.1.0/sys \
+#    -detached
+mkdir /tmp/log
+erl \
+    -sname cache \
+    -boot ./mspp \
+    -config ./sys \
+    -detached
