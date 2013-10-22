0e2429ba802b3fb66a35b8658232b4af3ae24193
bjtttt <bjguozhitao@163.com>
2013-10-22 20:30:30 +0800

    More debug

--- src/mon_data_parser.erl
+++ src/mon_data_parser.erl
@@ -533,27 +533,27 @@ get_link_info_reponse(_Req) ->
 				LinkPid =/= undefined ->
 					LinkPid ! {self(), count},
 					receive
-						{Num1, Num2, Num3, Num4, Num5, Num6, Num7, Num8, Num9, Num10} ->
-							Content = list_to_binary([<<46:?LEN_DWORD, 0:?LEN_BYTE, 21:?LEN_BYTE>>,
+						{Num1, Num2, Num3, Num4, Num5, Num6, Num7, Num8, Num9, Num10, Num11} ->
+							Content = list_to_binary([<<50:?LEN_DWORD, 0:?LEN_BYTE, 21:?LEN_BYTE>>,
 										<<Num1:?LEN_DWORD, Num2:?LEN_DWORD, Num3:?LEN_DWORD>>,
 										<<Num4:?LEN_DWORD, Num5:?LEN_DWORD, Num6:?LEN_DWORD>>,
-										<<Num7:?LEN_DWORD, Num8:?LEN_DWORD, Num9:?LEN_DWORD, Num10:?LEN_DWORD>>]),
+										<<Num7:?LEN_DWORD, Num8:?LEN_DWORD, Num9:?LEN_DWORD, Num10:?LEN_DWORD, Num11:?LEN_DWORD>>]),
 							Xor = vdr_data_parser:bxorbytelist(Content),
 							list_to_binary([Content, Xor])
 					end;
 				true ->
-					Content = list_to_binary([<<46:?LEN_DWORD, 0:?LEN_BYTE, 21:?LEN_BYTE>>,
+					Content = list_to_binary([<<50:?LEN_DWORD, 0:?LEN_BYTE, 21:?LEN_BYTE>>,
 								<<0:?LEN_DWORD, 0:?LEN_DWORD, 0:?LEN_DWORD>>,
 								<<0:?LEN_DWORD, 0:?LEN_DWORD, 0:?LEN_DWORD>>,
-								<<0:?LEN_DWORD, 0:?LEN_DWORD, 0:?LEN_DWORD, 0:?LEN_DWORD>>]),
+								<<0:?LEN_DWORD, 0:?LEN_DWORD, 0:?LEN_DWORD, 0:?LEN_DWORD, 0:?LEN_DWORD>>]),
 					Xor = vdr_data_parser:bxorbytelist(Content),
 					list_to_binary([Content, Xor])
 			end;
 		_ ->
-			Content = list_to_binary([<<46:?LEN_DWORD, 0:?LEN_BYTE, 21:?LEN_BYTE>>,
+			Content = list_to_binary([<<50:?LEN_DWORD, 0:?LEN_BYTE, 21:?LEN_BYTE>>,
 								<<0:?LEN_DWORD, 0:?LEN_DWORD, 0:?LEN_DWORD>>,
 								<<0:?LEN_DWORD, 0:?LEN_DWORD, 0:?LEN_DWORD>>,
-								<<0:?LEN_DWORD, 0:?LEN_DWORD, 0:?LEN_DWORD, 0:?LEN_DWORD>>]),
+								<<0:?LEN_DWORD, 0:?LEN_DWORD, 0:?LEN_DWORD, 0:?LEN_DWORD, 0:?LEN_DWORD>>]),
 			Xor = vdr_data_parser:bxorbytelist(Content),
 			list_to_binary([Content, Xor])
 	end.
